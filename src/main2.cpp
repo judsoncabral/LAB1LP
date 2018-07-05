a34967fe39e8905ca5fd9e8b549ee5c483b6a5cd
Judson Cabral <31037021+judsoncabral@users.noreply.github.com>
2018-07-05 18:25:54 -0300

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,11 +1,16 @@
 #include "../include/fatorial.h"
 #include "../include/primalidade.h"
+#include <stdexcept>
+
 #include <iostream>
 	using std::cin;
 	using std::cout;
 	using std::endl;
 
 int main(int argc,char *argv[]){
+	if(argc == 1){
+		throw std::runtime_error("Não foi fornecido um número!");
+	}
 	int x,y;
 	/*Chama as funcoes para achar o ultimo primo e achar fatorial*/
 	y = atoi(argv[1]);
@@ -13,5 +18,5 @@ int main(int argc,char *argv[]){
 
 	cout << "Maior número primo anterior ao fatorial de " << y << " (" << x << ") eh " << primalidade(x-1) << "." << endl; 
 
-
+	return EXIT_SUCCESS;
 }
